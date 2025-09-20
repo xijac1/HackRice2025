f18657b1e6be51c25f59a39df4128a49b5b15f49
S-khudairi <Samkhudairi2002@gmail.com>
2025-09-20 17:51:54 -0500

    Reversed the bottom Marquee

--- hackrice/registry/magicui/marquee.tsx
+++ hackrice/registry/magicui/marquee.tsx
@@ -39,10 +39,10 @@ export default function Marquee({
           <div
             key={i}
             className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
-              "animate-marquee flex-row": !vertical,
+              "animate-marquee flex-row": !vertical && !reverse,
+              "animate-marquee-reverse flex-row": !vertical && reverse,
               "animate-marquee-vertical flex-col": vertical,
               "group-hover:[animation-play-state:paused]": pauseOnHover,
-              "[animation-direction:reverse]": reverse,
             })}
           >
             {children}
